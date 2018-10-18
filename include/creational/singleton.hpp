be75720047e5e58d477e42d9eaae2e6ad87704a0
Elvis Oric <elvis.oric90@gmail.com>
2018-10-18 23:04:10 +0200

    Add singleton design pattern.

---
+++ include/creational/singleton.hpp
@@ -0,0 +1,18 @@
+#ifndef PATTERNS_CREATIONAL_SINGLETON_HPP
+#define PATTERNS_CREATIONAL_SINGLETON_HPP
+
+namespace patterns {
+namespace creational {
+
+template <typename Type>
+class Singleton {
+ public:
+  Type& instance() {
+    static Type instance = Type{};
+    return type;
+  }
+};
+
+}  // namespace creational
+}  // namespace patterns
+#endif  // PATTERNS_CREATIONAL_SINGLETON_HPP
